f390f1198162fc73ff25bd35cdcd5bb78c4c84c3
NovElena <novokhodskayaelena@gmail.com>
2026-03-16 16:47:06 +0200

    TakeScreenShot

--- src/test/java/ui_tests/LoginTests.java
+++ src/test/java/ui_tests/LoginTests.java
@@ -3,15 +3,18 @@ package ui_tests;
 import dto.User;
 import manager.AppManager;
 import org.testng.Assert;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import pages.ContactPage;
 import pages.HomePage;
 import pages.LoginPage;
 import utils.RetryAnalyzer;
+import utils.TestNGListener;
 
 import java.lang.reflect.Method;
 
 import static utils.PropertiesReader.*;
+@Listeners(TestNGListener.class)
 
 public class LoginTests extends AppManager {
     @Test(retryAnalyzer = RetryAnalyzer.class)
